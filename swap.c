0d99a1471528a1848f67f60bfe025989f7bd9da6
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-17 16:34:21 +0200

    modified swap

--- swap.c
+++ swap.c
@@ -10,32 +10,24 @@
 
 void swap(stack_t **stack, unsigned int line_nb)
 {
-	stack_t *swop = *stack;
-	int stack_nb = 0;
-	
-	if (!*stack || !(*stack)->next)
+	stack_t *tmp;
+		
+	if (!stack || !*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_nb);
 		is_error = 1;
 	}
 	else
 	{
-		while (swop && stack_nb < 2)
-		{
-			swop = swop->next;
-			stack_nb++;
-		}
-
-		if (stack_nb != 2)
-		{
-			fprintf(stderr, "L%d: can't swap, stack too short\n", line_nb);
-			is_error = 1;
-		}
-		else
-		{
-			stack_nb = swop->n;
-			swop->n = swop->next->n;
-			swop->next->n = stack_nb;
-		}
+		tmp = (*stack)->next;
+		(*stack)->next = tmp->next;
 	}
+
+	if (!tmp->next)
+		tmp->next->prev = *stack;
+
+	tmp->next = *stack;
+	(*stack)->prev = tmp;
+	tmp->prev = NULL;
+	*stack = tmp;
 }
