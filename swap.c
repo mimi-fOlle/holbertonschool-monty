f6a3f7f922e3148a2fe4a6a79b9ff12867aaaa85
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-17 16:40:12 +0200

    try fix seg fault

--- swap.c
+++ swap.c
@@ -12,7 +12,7 @@ void swap(stack_t **stack, unsigned int line_nb)
 {
 	stack_t *tmp;
 		
-	if (!stack || !*stack || !(*stack)->next)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_nb);
 		is_error = 1;
@@ -21,13 +21,15 @@ void swap(stack_t **stack, unsigned int line_nb)
 	{
 		tmp = (*stack)->next;
 		(*stack)->next = tmp->next;
+
+		if (!tmp->next)
+			tmp->next->prev = *stack;
+		else
+		{
+			tmp->next = *stack;
+			(*stack)->prev = tmp;
+			tmp->prev = NULL;
+			*stack = tmp;
+		}
 	}
-
-	if (!tmp->next)
-		tmp->next->prev = *stack;
-
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
-	tmp->prev = NULL;
-	*stack = tmp;
 }
