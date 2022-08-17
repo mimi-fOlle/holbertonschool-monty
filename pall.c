5ae5c67e3337ab675d1c238bb214df0de62575e7
MousElPsyCongroo <mtapha.tall@gmail.com>
2022-08-17 11:11:12 +0200

    Add newline to pall function printing

--- pall.c
+++ pall.c
@@ -13,7 +13,7 @@ void pall(stack_t **stack, __attribute__((unused)) unsigned int line_nb)
 
 	while (to_print)
 	{
-		printf("%d", to_print->n);
+		printf("%d\n", to_print->n);
 		to_print = to_print->next;
 	}
 }
