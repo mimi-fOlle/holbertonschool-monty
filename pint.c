9a54fee87366ecd9f042a019bb757aa4367ce728
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-17 11:55:48 +0200

    modify

--- pint.c
+++ pint.c
@@ -1,7 +1,25 @@
 #include "monty.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * pint - Prints the value at the top of the stack
+  * @stack: The stack
+  * @line_nb: The line number, unused
+  */
 
 void pint(stack_t **stack, unsigned int line_nb)
 {
-	printf("%d", line_nb);
+	stack_t *print_top;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L<%d>: can't pint, stack empty\n", line_nb);
+		is_error = 1;
+	}
+	while (print_top)
+	{
+		printf("%d\n", print_top->n);
+		print_top = print_top->next;
+	}
 }
